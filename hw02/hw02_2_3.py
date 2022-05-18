76f3d9c3439aa756ad343be7c805a6933ca8baa2
Max Timinski <timinmax@gmail.com>
2022-05-19 00:12:00 +0300

    PEP fix

--- hw02/hw02_2_3.py
+++ hw02/hw02_2_3.py
@@ -7,9 +7,9 @@ def check_int(item_to_check):
 
 
 def format_int(item_2_format):
-    #Эта функция под вопросом. Альтернативный вариант - приведение к int вставить внутрь функции replace
-    #И вместо вызова функции format_int вставить эту "формулу"
-    #КАК ЛУЧШЕ С Т.ЗРЕНИЯ BEST PRACTICE?
+    # Эта функция под вопросом. Альтернативный вариант - приведение к int вставить внутрь функции replace
+    # И вместо вызова функции format_int вставить эту "формулу"
+    # КАК ЛУЧШЕ С Т.ЗРЕНИЯ BEST PRACTICE?
     int_value = int(item_2_format)
     return item_2_format.replace(f"{int_value}", f"{int_value:02d}")
 
@@ -23,10 +23,10 @@ for digit_pos in digit_pos_list:
     init_list[digit_pos] = format_int(init_list[digit_pos])
     init_list.insert(digit_pos + 1, '"')
     init_list.insert(digit_pos, '"')
-#ВАРИАНТ 1
+# ВАРИАНТ 1
 print(" ".join(init_list))
 
-#ВАРИАНТ 2 - УБРАТЬ ЛИШНИЕ ПРОБЕЛЫ У КАВЫЧЕК
+# ВАРИАНТ 2 - УБРАТЬ ЛИШНИЕ ПРОБЕЛЫ У КАВЫЧЕК
 print(init_list)
 init_list = [f"{list_item}" if list_item == '"' or check_int(list_item) else f" {list_item} " for list_item in init_list]
 print(init_list)
